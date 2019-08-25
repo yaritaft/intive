28287f65e0b62fc6e57090ba388c690b24202fe7
yaritaft <yari.taft@gmail.com>
2019-08-25 01:12:05 -0300

    Tests added, Code coverage applied

--- classes.py
+++ classes.py
@@ -1,32 +1,46 @@
 class Rent:
     def __init__(self,amount_of_usage):
-        self.amount_of_usage=amount_of_usage
+        if (isinstance(amount_of_usage,int) or isinstance(amount_of_usage,float)):
+            self.amount_of_usage=amount_of_usage
+        else:
+            raise(TypeError("The amount of usage must be an int or float variable but the type set is: {}.".format(type(amount_of_usage))))
     def fee(self):
         return self.cost*self.amount_of_usage
 
 class HourRent(Rent):
     cost=5
     def __init__(self,amount_of_usage):
-        Rent.__init__(self,amount_of_usage)
+        super().__init__(amount_of_usage)
 class DayRent(Rent):
     cost=20
     def __init__(self,amount_of_usage):
-        Rent.__init__(self,amount_of_usage)
+        super().__init__(amount_of_usage)
 
 class WeekRent(Rent):
     cost=60
     def __init__(self,amount_of_usage):
-        Rent.__init__(self,amount_of_usage)
+        super().__init__(amount_of_usage)
 
 class FamilyRent:
     discount=0.3
     floor=3
     top=5
+
+    def validate_type_of_rents(self,list_of_rents):
+        return all(self.valid_rent(one_rent) for one_rent in list_of_rents)
+    def valid_rent(self,one_rent):
+        return ((isinstance(one_rent,WeekRent)) or (isinstance(one_rent,DayRent)) or (isinstance(one_rent,HourRent)))
+
     def __init__(self,rents):#ADD HERE TRY AND EXCEPT if we are not receiving a list
-        if (len(rents)>=self.floor) and (len(rents)<=self.top):#Apply controls to class type, every element should be hour, day or week rent and amount of rents
-            self.rents=rents
-        else:
-            print("error")
+        try:
+            if self.validate_type_of_rents(rents):
+                if (len(rents)>=self.floor) and (len(rents)<=self.top):#Apply controls to class type, every element should be hour, day or week rent and amount of rents
+                    self.rents=rents
+                else:
+                    raise(ValueError("The amount of rents is not between 3 and 5. You loaded {}".format(len(rents))))
+                    
+        except TypeError:
+            raise(TypeError("The type of variable provided to Family Rent is not a tuple or a list."))
     def apply_discounts_total(self,total):
         return total - (total*self.discount)
     def total_fee(self):
@@ -37,13 +51,4 @@ class FamilyRent:
         return total
 
 #Mencionar que al principio iba a usar polimorfmismo y terminé no usandolo porque no era necesario ya que el metodo era el mismo sisempre con distinta variable de clase.
-
-a=HourRent(5)
-b=DayRent(4)
-c=WeekRent(9)
-print(a.fee())
-print(b.fee())
-print(c.fee())
-d=FamilyRent((a,b,c))
-print(d.total_fee())
-
+#a=WeekRent("a")
---
+++ test.py
@@ -0,0 +1,45 @@
+import unittest
+from classes import HourRent,WeekRent,DayRent,Rent,FamilyRent
+
+class TestRents(unittest.TestCase):
+    def setUp(self):
+        self.an_hour_rent=HourRent(5)
+        self.a_day_rent=DayRent(4)
+        self.a_week_rent=WeekRent(9)
+
+    def test_hour_rent(self):
+        self.assertEqual(25,self.an_hour_rent.fee())
+
+    def test_week_rent(self):
+        self.assertEqual(80,self.a_day_rent.fee())
+    def test_day_rent(self):
+        self.assertEqual(540,self.a_week_rent.fee())
+    def test_hour_rent_with_type_error(self):
+        self.assertRaises(TypeError,HourRent,"a")
+    def test_day_rent_with_type_error(self):
+        self.assertRaises(TypeError,DayRent,"a")
+    def test_week_rent_with_type_error(self):
+        self.assertRaises(TypeError,WeekRent,"a")
+class TestFamilyRents(unittest.TestCase):
+    def setUp(self):
+        self.an_hour_rent=HourRent(5)
+        self.a_day_rent=DayRent(4)
+        self.a_week_rent=WeekRent(9)
+        self.different_family_rents=FamilyRent((self.an_hour_rent,self.a_day_rent,self.a_week_rent))
+        self.equal_family_rents=FamilyRent((self.an_hour_rent,self.an_hour_rent,self.an_hour_rent))
+    def test_family_rent_different_rents(self):
+        self.assertEqual(451.5,self.different_family_rents.total_fee())
+
+    def test_family_rent_equal_rents(self):
+        self.assertEqual(52.5,self.equal_family_rents.total_fee())
+    def test_family_rent_less_amount_of_rents(self):
+        self.assertRaises(ValueError,FamilyRent,(self.a_day_rent,))
+    def test_family_rent_more_amount_of_rents(self):
+        self.assertRaises(ValueError,FamilyRent,(self.a_day_rent,self.a_day_rent,self.a_day_rent,self.a_day_rent,self.a_day_rent,self.a_day_rent))
+    def test_family_rent_no_list_of_rents(self):
+        self.assertRaises(TypeError,FamilyRent,5)
+
+
+
+if __name__=="__main__":
+    unittest.main()
